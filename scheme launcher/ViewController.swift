a75cc450c96755c098d3818dc15129632b82f6e4
lomudi <lomudi@gmail.com>
2018-02-13 15:30:49 +0200

    Initial Commit

---
+++ scheme launcher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  scheme launcher
+//
+//  Created by Udi Lumnitz on 13/02/2018.
+//  Copyright © 2018 Udi Lumnitz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
